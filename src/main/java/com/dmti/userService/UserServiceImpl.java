991c7add194d4de2e01564ce71ee44e5c2fdef42
xujun95 <1150329009@qq.com>
2019-12-06 00:12:22 +0800

    提交

--- src/main/java/com/dmti/userService/UserServiceImpl.java
+++ src/main/java/com/dmti/userService/UserServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @auther XuJun
@@ -18,16 +20,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Object login(String name, String password) {
-        User user = jdbctemplate.queryForObject("SELECT * FROM sys_user WHERE name = " + name + " and password = " + password, User.class);
-        if (user != null) {
-            return new Msg(1,user);
+        List<Map<String,Object>> list = jdbctemplate.queryForList("SELECT * FROM sys_user WHERE name = '" + name + "' and password = '" + password+"'");
+        if (list != null) {
+            return new Msg(1,list.get(0));
         }
         return new Msg(0);
     }
 
     @Override
     public Object register(User user) {
-        String sql = "INSERT INTO sys_user(name,password,nickname)VALUES(" + user.getName() + "," + user.getPassword() + "," + user.getNickname() + ")";
+        String sql = "INSERT INTO sys_user(name,password,nickname)VALUES('" + user.getName() + "','" + user.getPassword() + "','" + user.getNickname() + "')";
         if (jdbctemplate.update(sql) != 0) {
             return new Msg(1);
         }
@@ -36,7 +38,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Object updatePassword(User user) {
-        String sql = "UPDATE sys_user SET `password` = "+user.getPassword()+" WHERE id = "+user.getId();
+        String sql = "UPDATE sys_user SET `password` = '"+user.getPassword()+"' WHERE id = '"+user.getId()+"'";
         if (jdbctemplate.update(sql) != 0) {
             return new Msg(1);
         }
